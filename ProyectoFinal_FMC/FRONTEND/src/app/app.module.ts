1f49b09f57605629695dca66cb3da409e7d8d460
FrancoMCuello <franco.m.cuello@gmail.com>
2022-07-27 10:38:29 -0300

    Final de Front

--- ProyectoFinal_FMC/FRONTEND/src/app/app.module.ts
+++ ProyectoFinal_FMC/FRONTEND/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Componentes/header/header.component';
 import { LogosComponent } from './Componentes/logos/logos.component';
+import { VideoComponent } from './Componentes/video/video.component';
+import { AcercaDeComponent } from './Componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './Componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './Componentes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './Componentes/hys/hys.component';
+import { ProyectosComponent } from './Componentes/proyectos/proyectos.component';
+import { FooterComponent } from './Componentes/footer/footer.component';
+
 
 
 
