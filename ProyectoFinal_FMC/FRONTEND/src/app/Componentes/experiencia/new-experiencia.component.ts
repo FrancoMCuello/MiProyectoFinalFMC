ab62f07c0f95b382c176827e8c2b2d93758d635c
FrancoMCuello <franco.m.cuello@gmail.com>
2022-09-27 14:36:52 -0300

    Reparando Componente Experiencia

--- ProyectoFinal_FMC/FRONTEND/src/app/Componentes/experiencia/new-experiencia.component.ts
+++ ProyectoFinal_FMC/FRONTEND/src/app/Componentes/experiencia/new-experiencia.component.ts
@@ -11,6 +11,8 @@ import { SExperienciaService } from 'src/app/service/s-experiencia.service';
 export class NewExperienciaComponent implements OnInit {
   nombreE: string = '';
   descripcionE: string = '';
+  fechaFE: string = '';
+  fechaIE: string = '';
 
   constructor(private sExperiencia: SExperienciaService, private router: Router) { }
 
@@ -18,7 +20,7 @@ export class NewExperienciaComponent implements OnInit {
   }
 
   onCreate(): void{
-    const expe = new Experiencia(this.nombreE, this.descripcionE);
+    const expe = new Experiencia(this.nombreE, this.descripcionE, this.fechaFE, this.fechaIE);
     this.sExperiencia.save(expe).subscribe(
       data=>{ 
         alert("Experiencia añadida");
